225ad9d805ac18e337c6bf801fcb4f9dc09ea5d7
supspitdamn <pyatkov.dim@gmail.com>
2026-04-28 10:56:52 +0600

    Переход на PostgreSQL

--- api/database.py
+++ api/database.py
@@ -1,6 +1,13 @@
-import sqlite3
+import psycopg2
+# использование postgre обусловлено возможным большим потоков запросов к БД
 
-DATA_BASE = "SCUDA_INFO"
+DB_CONFIG = {
+    "dbname": "SCUDA_INFO",
+    "user": "admin",
+    "password": "75egve57",
+    "host": "localhost", # Запуск на локальной машине
+    "port": 5432 # стандартный порт для postgre
+}
 
 def execute_connection():
 
@@ -10,7 +17,7 @@ def execute_connection():
 
     try:
 
-        conn = sqlite3.connect(DATA_BASE)
+        conn = psycopg2.connect(**DB_CONFIG)
         return conn
     
     except Exception as e:
@@ -33,19 +40,29 @@ def db_init():
             crs = conn.cursor()
 
             crs.execute("""
+                        CREATE TABLE IF NOT EXISTS roles (
+                        id SERIAL PRIMARY KEY,
+                        role_name TEXT NOT NULL UNIQUE,
+                        access_level INTEGER NOT NULL
+                        )
+                        """)
+
+            crs.execute("""
                 CREATE TABLE IF NOT EXISTS employees (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    card_id TEXT NOT NULL,
+                    id SERIAL PRIMARY KEY,
+                    card_id TEXT NOT NULL UNIQUE,
                     full_name TEXT NOT NULL,
                     department TEXT NOT NULL,
-                    entry_level INTEGER NOT NULL
+                    role_id INTEGER NOT NULL,
+                    is_active INTEGER DEFAULT 1,
+                    FOREIGN KEY (role_id) REFERENCES roles (id)
                 )
             """)
 
             crs.execute("""
                 CREATE TABLE IF NOT EXISTS rooms (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    room_number INTEGER NOT NULL,
+                    id SERIAL PRIMARY KEY,
+                    room_number INTEGER NOT NULL UNIQUE,
                     description TEXT,
                     entry_level INTEGER NOT NULL
                 )
@@ -53,20 +70,20 @@ def db_init():
 
             crs.execute("""
                 CREATE TABLE IF NOT EXISTS access_points (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id SERIAL PRIMARY KEY,  
                     room_id INTEGER NOT NULL,
                     entrance_name TEXT NOT NULL,
                     direction TEXT CHECK (direction IN ('IN', 'OUT', 'BOTH')),
-                    FOREIGN KEY (room_id) REFERENCES rooms (id)
+                    FOREIGN KEY (room_id) REFERENCES rooms (id) ON DELETE CASCADE
                 )
             """)
 
             crs.execute("""
                 CREATE TABLE IF NOT EXISTS access_logs (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id SERIAL PRIMARY KEY,
                     employee_id INTEGER NOT NULL,
-                    access_point_id INTEGER NOT NULL, -- Заменили room_id
-                    event_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    access_point_id INTEGER NOT NULL,
+                    event_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                     is_granted INTEGER NOT NULL,
                     FOREIGN KEY (employee_id) REFERENCES employees (id),
                     FOREIGN KEY (access_point_id) REFERENCES access_points (id)
@@ -79,6 +96,7 @@ def db_init():
         except Exception as e:
 
             print(f"Инициализация провалена: {e}")
+            conn.rollback()
 
         finally:
 
@@ -86,23 +104,17 @@ def db_init():
 
 def db_clear():
     """
-    Очистка базы данных
+    Очистка базы данных (TRUNCATE быстрее для больших таблиц)
     """
-
     conn = execute_connection()
-
-    try:
-
-        crs = conn.cursor()
-
-        crs.execute("""DELETE FROM employees""")
-
-        crs.execute("""DELETE FROM rooms""")
-
-        crs.execute("""DELETE FROM access_points""")
-
-        crs.execute("""DELETE FROM access_logs""")
-
-    except Exception as e:
-
-        print(f"Ошибка в очистке базы данных. Детали: {e}")
+    if conn:
+        try:
+            crs = conn.cursor()
+            crs.execute("TRUNCATE TABLE access_logs, access_points, rooms, employees, roles CASCADE;")
+            conn.commit()
+            print("База данных очищена.")
+        except Exception as e:
+            print(f"Ошибка в очистке базы данных. Детали: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
