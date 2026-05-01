82e9fd4348d9ddcbb2bd10e6f0269a80089dec04
supspitdamn <pyatkov.dim@gmail.com>
2026-05-01 15:24:18 +0600

    Обновлен список схем

--- api/schemas.py
+++ api/schemas.py
@@ -1,26 +1,85 @@
 import pydantic
 import uuid 
 from datetime import datetime
+from enum import Enum
+
+class AccessGroupCreate(pydantic.BaseModel):
+    group_name: str
+
+class AccessGroup(AccessGroupCreate):
+    id: int
+
+class EmployeeAccessGroupCreate(pydantic.BaseModel):
+    employee_id: int
+    group_id : int
+
+class EmployeeAccessGroup(EmployeeAccessGroupCreate):
+    id : int
+
+class RoomGroupCreate(pydantic.BaseModel):
+    group_id: int
+    room_id: int
+
+class RoomGroup(RoomGroupCreate):
+    id: int
+
+class DirectionsEnum(str, Enum):
+    IN = "IN"
+    OUT = "OUT"
+    BOTH = "BOTH"
+
+class RoleCreate(pydantic.BaseModel):
+    role_name: str
+    access_level: int
+
+class Role(RoleCreate):
+    id: int
 
 class EmployeeCreate(pydantic.BaseModel):
-
     card_id: str
     full_name: str
     department: str
-    entry_level: int
+    role_id: int
+    is_active: bool = True
 
-class Employee(pydantic.BaseModel):
-
+class Employee(EmployeeCreate):
     id: int
 
 class RoomCreate(pydantic.BaseModel):
-
     room_number: int
     description: str = None
     entry_level: int
 
-class CreateAccessPoint(pydantic.BaseModel):
+class Room(RoomCreate):
+    id: int
 
+class AccessPointCreate(pydantic.BaseModel):
     room_id: int
     entrance_name: str
-    direction: str
+    direction: DirectionsEnum
+
+class AccessPoint(AccessPointCreate):
+    id: int
+
+class AccessPointResponse(pydantic.BaseModel):
+    id: int
+    employee_id: int
+    access_point_id: int
+    event_time: datetime
+    is_granted: bool
+
+class StatusResponse(pydantic.BaseModel):
+    status: str = "Неизвестно"
+    message: str = "..."
+
+class LogEntry(pydantic.BaseModel):
+    id: int
+    employee_name: str
+    room_number: int
+    entrance: str
+    time: str
+    is_granted: bool
+
+class ViewLogsResponse(pydantic.BaseModel):
+    status: str
+    data: list[LogEntry]
